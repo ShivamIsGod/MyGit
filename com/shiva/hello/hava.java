a373f73b6911b8dfad658df624b1b909921a3d06
Shivam-shivam0911 <krshivam2206@gmail.com>
2025-03-13 00:23:53 +0530

    Everything is deleted

--- com/shiva/hello/hava.java
+++
@@ -1,8 +0,0 @@
-package com.shiva.hello;
-
-public class hava {
-    public static void main(String[] args) {
-        System.out.println("hello java");
-        System.out.println("java is good");
-    }
-}
