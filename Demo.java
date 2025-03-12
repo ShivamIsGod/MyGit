37eb61690bb6c09b0b51d5b276bf7df519bcffee
Shivam-shivam0911 <krshivam2206@gmail.com>
2025-03-13 01:13:05 +0530

    updated

--- Demo.java
+++ Demo.java
@@ -1,9 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-        int x=100;
-        System.out.println("Hello world");
-        System.out.println("in  bug fix branch");
-        System.out.println("comparing bug fix and amin thsi should not go to main without PR");
+        System.out.println("We are i bugfix branch");
         
     }
 }
