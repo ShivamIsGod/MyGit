3ca9efaf5a0b643ddd54913b42240a4fb0576767
Shivam-shivam0911 <krshivam2206@gmail.com>
2025-03-13 00:56:26 +0530

    commeted

--- Demo.java
+++ Demo.java
@@ -2,8 +2,8 @@ public class Demo {
     public static void main(String[] args) {
         int x=100;
         System.out.println("Hello world");
-        System.out.println("in  bug fix branch");
-        System.out.println("comparing bug fix and amin thsi should not go to main without PR");
+        System.out.println("we are here in main");
+
         
     }
 }
