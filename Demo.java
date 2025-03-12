a5795c89f183f3344bb4000494cef2687b26e3d3
Shivam-shivam0911 <krshivam2206@gmail.com>
2025-03-13 01:14:43 +0530

    delete some lines

--- Demo.java
+++ Demo.java
@@ -1,9 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-        int x=100;
-        System.out.println("Hello world");
         System.out.println("we are here in main");
 
-        
     }
 }
